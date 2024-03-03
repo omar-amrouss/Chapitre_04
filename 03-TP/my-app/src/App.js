25b25783e26616fab934ad27c3f2f9766ee832cb
omar-amrouss <omar.amrouss@outlook.be>
2024-03-03 17:09:08 +0100

    second commit

--- 03-TP/my-app/src/App.js
+++ 03-TP/my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const variable = "React";
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn {variable}{2 * 9}
         </a>
       </header>
     </div>
